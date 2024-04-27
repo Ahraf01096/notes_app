1d43c8476ea2ba6e8d15cbbb9704f03fc72f5cef
Ahraf01096 <ahmed.ashraf27163@gmail.com>
2024-04-27 19:19:16 +0300

    create edit note view and navigate to it

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,46 +1,55 @@
 import 'package:flutter/material.dart';
 
+import 'edit_note_view.dart';
+
 class NoteItem extends StatelessWidget {
    NoteItem({required this.color,super.key});
  final Color color ;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16), color: color),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            contentPadding: const EdgeInsets.all(0),
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
-            ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 10),
-              child: Text(
-                'Build your carer with ahmed ashraf',
-                style:
-                    TextStyle(color: Colors.black.withOpacity(.5), fontSize: 16),
+    return GestureDetector(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteView();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16), color: color),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              contentPadding: const EdgeInsets.all(0),
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Text(
+                  'Build your carer with ahmed ashraf',
+                  style:
+                      TextStyle(color: Colors.black.withOpacity(.5), fontSize: 16),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  color: Colors.black,
+                  size: 30,
+                ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                color: Colors.black,
-                size: 30,
-              ),
-            ),
-          ),
-          const SizedBox(height: 16,),
-          Text(
-            'Apr 26/2024',
-            style: TextStyle(color: Colors.black.withOpacity(.5)),
-          )
-        ],
+            const SizedBox(height: 16,),
+            Text(
+              'Apr 26/2024',
+              style: TextStyle(color: Colors.black.withOpacity(.5)),
+            )
+          ],
+        ),
       ),
     );
   }
